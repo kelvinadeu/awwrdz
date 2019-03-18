7b0e11e02b6cf06463c5b678f2853baf400110e3
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-18 13:53:52 +0300

    added class model profile

--- awwards/models.py
+++ awwards/models.py
@@ -8,4 +8,20 @@ class Profile(models.Model):
     profile_photo = models.ImageField(upload_to = 'pics/')
     bio = models.TextField(max_length=100,blank=True)
     user_id = models.OneToOneField(User,on_delete=models.CASCADE)
-    
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def save_user(self):
+        self.save()
+
+    @classmethod
+    def search_user(cls,username):
+        searched_user = User.objects.get(username = username)
+        return searched_user
+
+    def __str__(self):
+        return self.user.username
