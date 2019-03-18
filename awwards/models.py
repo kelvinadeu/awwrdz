78d35fc38dd93eac9815107a8af16e58092b2f43
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-18 12:32:53 +0300

    added model class

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length =30,null=True)
+    profile_photo = models.ImageField(upload_to = 'pics/')
+    bio = models.TextField(max_length=100,blank=True)
+    user_id = models.OneToOneField(User,on_delete=models.CASCADE)
+    
