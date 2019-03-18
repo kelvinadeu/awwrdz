140a59536bda01c583e482fd25432748b040f404
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-18 11:22:14 +0300

    added sign up

--- awwards/views.py
+++ awwards/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
 def signup(request):
-    return render(request,'registration/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('login')
+
+    else:
+        form = UserCreationForm()
+    return render(request,'signup.html',{
+        "form": form
+    })    
