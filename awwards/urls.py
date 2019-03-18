157276223e5860a43b5074529800300050fa5777
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-18 18:22:39 +0300

    added home.html

--- awwards/urls.py
+++ awwards/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls import url,include
 from . import views
 
 urlpatterns = [
-    url('signup/',views.signup, name='signup'),
+    url('',views.signup, name='signup'),
     url('login/',views.login, name='login'),
-    url(r'accounts/', include('django.contrib.auth.urls'))
+    # url(r'accounts/', include('django.registration.auth.urls'))
 
     # url('admin/',admin.site.urls)
 
