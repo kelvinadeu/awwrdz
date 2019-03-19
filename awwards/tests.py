485bb15ea388e0c7febac0c46599edb22a607e45
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-19 19:46:20 +0300

    added test

--- awwards/tests.py
+++ awwards/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Project,UserProfile
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.new_user = User.objects.create_user(username='user',password='user-password')
+        self.new_profile = UserProfile(id=1,user=self.new_user,profile_pic='photos/pic',bio='user-bio')
