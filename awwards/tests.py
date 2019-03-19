7cbac331b87e9ad01277fb70cc90ac9339e8a1b8
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-19 19:59:44 +0300

    added test

--- awwards/tests.py
+++ awwards/tests.py
@@ -6,4 +6,8 @@ from .models import Project,UserProfile
 class ProfileTestClass(TestCase):
     def setUp(self):
         self.new_user = User.objects.create_user(username='user',password='user-password')
-        self.new_profile = UserProfile(id=1,user=self.new_user,profile_pic='photos/pic',bio='user-bio')
+        self.new_profile = UserProfile(id=1,user=self.new_user,bio='user-bio')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,UserProfile))
+    
